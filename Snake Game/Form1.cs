a6cb4d8653bc87112f0733eecd8ab5dfe66a072d
drumil <drumil@dvd.(none)>
2014-01-09 00:56:02 +0530

    first commit

---
+++ Snake Game/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Graphics paper;
+        Snake snake = new Snake();
+        Random randFood = new Random();
+        Food food;
+
+        bool left = false;
+        bool right = false;
+        bool up = false;
+        bool down = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            food = new Food(randFood);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            paper = e.Graphics;
+            food.drawFood(paper);
+            snake.drawSnake(paper);
+            
+            for (int i = 0; i < snake.snakeRec.Length; i++)
+            {
+                if(snake.snakeRec[i].IntersectsWith(food.foodRec))
+                {
+                    food.foodLocation(randFood);
+                    food.drawFood(paper);
+                    snake.length++;
+                }
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Down && up == false)
+            {
+                down = true;
+                up = false;
+                left = false;
+                right = false;
+            }
+            else if (e.KeyData == Keys.Up && down == false)
+            {
+                down = false;
+                up = true;
+                left = false;
+                right = false;
+            }
+            else if (e.KeyData == Keys.Left && right == false)
+            {
+                down = false;
+                up = false;
+                left = true;
+                right = false;
+            }
+            else if (e.KeyData == Keys.Right && left == false)
+            {
+                down = false;
+                up = false;
+                left = false;
+                right = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (down) { snake.moveDown(); }
+            if (up) { snake.moveUp(); }
+            if (left) { snake.moveLeft(); }
+            if (right) { snake.moveRight(); }
+            this.Invalidate();
+        }
+    }
+}
---
+++ Snake Game/Snake.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WindowsFormsApplication1
+{
+    public class Snake
+    {
+        public Rectangle[] snakeRec;
+        private SolidBrush brush;
+        private int i,x, y, width, height;
+        public int length=3;
+
+        public Snake()
+        {
+            snakeRec = new Rectangle[length];
+            brush = new SolidBrush(Color.Blue);
+            x = 20;
+            y = 0;
+            width = 10;
+            height = 10;
+
+            for (i = 0; i < snakeRec.Length; i++)
+            {
+                snakeRec[i] = new Rectangle(x, y, width, height);
+                x -= 10;
+            }
+        }
+
+        public void drawSnake(Graphics paper)
+        {
+            foreach (Rectangle rec in snakeRec)
+            {
+                paper.FillRectangle(brush, rec);
+            }
+        }
+
+        public void drawSnake()
+        {
+            for (int i = snakeRec.Length - 1; i > 0; i--)
+            {
+                snakeRec[i] = snakeRec[i - 1];
+            }
+        }
+
+        public void moveDown()
+        {
+            drawSnake();
+            snakeRec[0].Y += 10;
+        }
+        public void moveUp()
+        {
+            drawSnake();
+            snakeRec[0].Y -= 10;
+        }
+        public void moveLeft()
+        {
+            drawSnake();
+            snakeRec[0].X -= 10;
+        }
+        public void moveRight()
+        {
+            drawSnake();
+            snakeRec[0].X += 10;
+        }
+    }
+}
